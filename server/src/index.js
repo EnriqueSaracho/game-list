490b3453b96fc7f470cba68b8d9385d449bcbd09
Enrique Saracho Felix <esarachof97@gmail.com>
2023-03-20 17:44:08 -0700

    index.js Users.js users.js package.json modified
    connection to database successful

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { config } from "dotenv";
+config({ path: "./config.env" });
+
+import { userRouter } from "./routes/users.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
+
+const uri = process.env.URI;
+
+mongoose.connect(uri);
 
 app.listen(3001, () => console.log("SERVER STARTED"));
