6b79ee4715a6da34097a6baccc9539fc6dab5434
Enrique Saracho Felix <esarachof97@gmail.com>
2023-03-31 14:22:20 -0700

    client/ modified
    created pages files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Auth } from "./pages/auth";
+import { AddGame } from "./pages/add-game";
+import { SavedGames } from "./pages/saved-games";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/add-game" element={<AddGame />} />
+          <Route path="/saved-games" element={<SavedGames />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/add-game.js
@@ -0,0 +1,3 @@
+export const AddGame = () => {
+  return <div>AddGame</div>;
+};
